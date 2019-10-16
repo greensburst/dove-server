36264248e3d771fb5925a71eec9d383ae560836b
lisuqing <greensburst@163.com>
2019-10-16 21:30:17 +0800

    可以回复前端了

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 )
+
+type User struct {
+	Nickname string `json:"nickname"`
+	Mail     string `json:"mail"`
+	Password string `json:"password"`
+}
 
 func process(conn net.Conn) {
 	defer conn.Close()
@@ -16,7 +23,32 @@ func process(conn net.Conn) {
 			fmt.Println("客户端退出")
 			return
 		}
-		fmt.Print(string(buf[:n]))
+		if string(buf[:n]) == "islogin" {
+			res := "yes"
+			buf := []byte(res)
+			n, err = conn.Write(buf)
+			if err != nil {
+				fmt.Println("服务器错误")
+			}
+		} else {
+			user := new(User)
+			json.Unmarshal(buf[:n], user)
+			if user.Mail == "116@qq.com" && user.Password == "123" {
+				res := "access"
+				buf := []byte(res)
+				n, err = conn.Write(buf)
+				if err != nil {
+					fmt.Println("服务器错误")
+				}
+			} else {
+				res := "error"
+				buf := []byte(res)
+				n, err = conn.Write(buf)
+				if err != nil {
+					fmt.Println("服务器错误")
+				}
+			}
+		}
 	}
 }
 
